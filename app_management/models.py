b93e74d42f17d953a860bd185814cf81c6d4b8c5
halex22 <73904950+halex22@users.noreply.github.com>
2023-08-01 15:19:04 +0200

    init dunder deleted from the models. add the receiver decorator for better data persintence

--- app_management/models.py
+++ app_management/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.dispatch import receiver
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import datetime
 
 class Artist(models.Model):
     """
     The model of an artist to be added to the database
+    params when using form: name, genre fundation_date
     """
     name = models.CharField(max_length=50)
     genre = models.CharField(max_length=25)
@@ -20,21 +22,26 @@ class Artist(models.Model):
     )
     ], null=True, blank=True)
 
-    def __init__(self, name: str, genre: str, fundation_date: int ) -> None:
-        self.name = name
-        self.genre = genre
-        self.fundation_date = fundation_date
-
-    def __repr__(self) -> str:
-        return f"{self.name}"
+    def __str__(self):
+        return self.name
 
 class Album(models.Model):
+    """
+    the model of an album to be added to the database
+    params when using form: name, released_date, artist
+    """
     name = models.CharField(max_length=50)
     released_date = models.DateField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
     
-    def __init__(self, name: str) -> None:
-        self.name = name
+    def __str__(self) -> str:
+        return self.name
 
-    def __repr__(self) -> str:
-        return f"{self.name}"
+@receiver(models.signals.pre_save, sender=Artist)
+def artist_pre_save(sender, instance, **kwargs):
+    instance.name = instance.name.lower()
+    instance.genre = instance.genre.lower()
+
+@receiver(models.signals.pre_save, sender=Album)
+def artist_pre_save(sender, instance, **kwargs):
+    instance.name = instance.name.lower()
--- app_management/views.py
+++ app_management/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .froms import ArtistForm
+from .forms import ArtistForm
+from .models import Artist
 
 
 def home(request):
@@ -10,7 +11,12 @@ def add_artist(request):
     if request.method == "POST":
         form = ArtistForm(request.POST)
         if form.is_valid():
-            print("ok")
+            artist = Artist(
+                name= form.cleaned_data["artist_name"],
+                genre= form.cleaned_data["genre"],
+                fundation_date= form.cleaned_data["fundation_date"]
+            )
+            artist.save()            
     else:
         form = ArtistForm()
     context = {"form": form}
